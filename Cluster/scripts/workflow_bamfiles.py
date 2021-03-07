1990260572b9829426b22e07db0aabafe02f9732
kaspermunch <kaspermunch@birc.au.dk>
2021-03-07 11:47:24 +0100

    Updated bam workflow

--- Cluster/scripts/workflow_bamfiles.py
+++ Cluster/scripts/workflow_bamfiles.py
@@ -29,16 +29,25 @@ def bam_index(path):
     spec = f'samtools index {path}'
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
-def bam_subset(path, index):
+def bam_region(path, index):
     sample_name = os.path.basename(os.path.dirname(path))
     output_path = modpath(path, base=sample_name, suffix='.region.bam')
-    # output_path = modpath(path, base=sample_name, suffix='.chr2.bam')
     inputs = {'path': path, 'index': index}
     outputs = {'path': output_path}
     options = {'memory': '4g',
                'walltime': '0-02:00:00'}
+    # spec = f'samtools view -f 3 -F 4 -b -h {path} -o {output_path} 2:135000000-145000000'
     spec = f'samtools view -b -h {path} -o {output_path} 2:135000000-145000000'
-    # spec = f'samtools view -f 3 -F 4 -b -h {path} -o {output_path} 2'
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+def bam_chr2(path, index):
+    sample_name = os.path.basename(os.path.dirname(path))
+    output_path = modpath(path, base=sample_name, suffix='.chr2.bam')
+    inputs = {'path': path, 'index': index}
+    outputs = {'path': output_path}
+    options = {'memory': '4g',
+               'walltime': '0-02:00:00'}
+    spec = f'samtools view -f 3 -F 4 -b -h {path} -o {output_path} 2'
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
 def bam2fastq(path):
@@ -62,14 +71,25 @@ bam_index_targets = gwf.map(bam_index, bam_files)
 
 # subset bam files
 bam_index_files = [d['path'] for d in bam_index_targets.outputs]
-bam_subset_files = []
+bam_region_files = []
+bam_chr2_files = []
 for i, (bam_file, bam_index_file) in enumerate(zip(bam_files, bam_index_files)):
-    target = gwf.target_from_template(f'bam_subset_{i}', bam_subset(path=bam_file, index=bam_index_file))
-    bam_subset_files.append(target.outputs['path'])
+
+    target = gwf.target_from_template(f'bam_region_{i}', bam_region(path=bam_file, index=bam_index_file))
+    bam_region_files.append(target.outputs['path'])
+
+    target = gwf.target_from_template(f'bam_chr2_{i}', bam_chr2(path=bam_file, index=bam_index_file))
+    bam_chr2_files.append(target.outputs['path'])
+
+
+bam_chr2_index_targets = gwf.map(bam_index, bam_chr2_files, name='bam_chr2_index')
+
+
+
 #bam_subset_targets = gwf.map(bam_subset, bam_files)
 
 # make fastq files
-bam2fastq_targets = gwf.map(bam2fastq, bam_subset_files)
+bam2fastq_targets = gwf.map(bam2fastq, bam_region_files)
 
 
 
